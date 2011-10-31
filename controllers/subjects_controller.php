99e45f3ec0a0aed764ba0d595d11cfd7eeddf0fa
Ajay Gavankar <ajygvnkr@gmail.com>
2011-10-31 20:32:28 +0530

    changed to make finding teacher name easier

--- controllers/subjects_controller.php
+++ controllers/subjects_controller.php
@@ -60,14 +60,18 @@ class SubjectsController extends AppController {
 	}
 	
 	function subjects() {
-             $user=($this -> Session -> read("Auth.User"));
-             $q='"';
-             $class= $q.$user ['class'].$q;
-             $id=$user['id'];
-               $subjects=$this->Subject->query("SELECT * FROM subjects WHERE class = $class  AND id NOT IN ( SELECT subject_id FROM subject_memberships WHERE student_id = $id AND form_a_submitted = 1 AND form_a_submitted = 1 );");
-                $this->set('subjects',$subjects);
-	
-                
+             $user = $this->Session->read("Auth.User");
+             $subjects = $this->Subject->find('all', array(
+             	'conditions' => array('Subject.class' => $user ['class']),
+             	'not' => $this->SubjectMembership->find('list',
+             			'conditions'=> array(
+             				'student_id' => $user[id],
+             				'form_a_submitted' => '1',
+             				'form_b_submitted' => '1'
+             			),
+             		);
+             ));
+             $this->set('subjects',$subjects);
         }
         
 }
