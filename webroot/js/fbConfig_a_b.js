04ccaf44b9d585ae77658c57865ef5da2f94dddd
Arun Bharadwaj <arun.ub29@gmail.com>
2011-12-19 13:34:04 +0530

    new files

---
+++ webroot/js/fbConfig_a_b.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+  $(document).ready(function() {
+           $(".modal").fancybox({
+				'width'				: '38%',
+				'height'			: '104%',
+				'autoScale'			: false,
+                                'scrolling':'no',
+				'transitionIn'		: 'none',
+				'transitionOut'		: 'none',
+                                'padding':'0',
+                                'titlePosition':'over',
+                                'overlayColor':'#fff',
+				'type'				: 'iframe'
+			
+			});
+
+			
+            });
