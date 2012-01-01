04e7b77939b230d402a06018e1b25c9b428bc855
Ajay Gavankar <ajygvnkr@gmail.com>
2012-01-01 13:18:22 +0530

    modified to handle multiple submissions

--- controllers/form_a_records_controller.php
+++ controllers/form_a_records_controller.php
@@ -38,6 +38,7 @@ class FormARecordsController extends AppController {
                         $this->data['FormARecord']['subject_id']=  $param['subject_id'];
                         $this->data['FormARecord']['teacher']=  $param['teacher'];
                         $this->data['FormARecord']['student']=  $param['student'];
+			$this->data['FormARecord']['submission_number']= $param['sub_num'];
 			if ($this->FormARecord->save($this->data)) {
 				$this->Session->setFlash(__('The form a record has been saved', true));
 				$this->redirect(array('controller' => 'Subjects', 'action' => 'subjects'));
@@ -45,11 +46,19 @@ class FormARecordsController extends AppController {
 				$this->Session->setFlash(__('The form a record could not be saved. Please, try again.', true));
 			}
 		}else{
-                	$user=($this->Session->read("Auth.User"));
                 	$params['subject_id']=$param[0];
                 	$params['teacher']=$param[1];
-                	$params['student']=$user['id'];
-                	$this -> Session ->write('params',$params);
+                	$params['student']=$this->Session->read("Auth.User.id");;
+                	$temp=$this->FormARecord->find('first',array(
+                		'fields' => 'ifnull(max(submission_number),0) as sub_num',//set sub_num to 0 if it is null
+                		'conditions' => array(
+                			'subject_id' => $params['subject_id'],
+                			'teacher' => $params['teacher'],
+                			'student' => $params['student']
+                		)
+                	));
+                	$params['sub_num']=$temp[0]['sub_num']+1;//set sub_num to next submission_number value
+                	$this->Session->write('params',$params);
                 }
 	}
 	
