d3c27554a2087edd4d6a4d1e2d36aef68587be38
Arun Bharadwaj <arun.ub29@gmail.com>
2012-01-26 17:03:13 +0530

    Changes

--- config/feedback.php
+++ config/feedback.php
@@ -1,4 +1,4 @@
 <?php
-$config['max_sub_num'] =1;//the maximum number of submissions for feedback that the system will take at this point
+$config['max_sub_num'] =3;//the maximum number of submissions for feedback that the system will take at this point
 $config['enable_mails'] = true;//whether emails can be sent by the system or not
 ?>
